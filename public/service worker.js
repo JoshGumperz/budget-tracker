a4e4f85e5fc977a05c8028df2cee574e3d93d615
Josh Gumperz <joshg3023@gmail.com>
2021-08-20 00:59:26 -0700

    added event listener on install and activate in service-worker.js

--- public/service worker.js
+++ public/service worker.js
@@ -10,3 +10,32 @@ const FILES_TO_CACHE = [
 
 const CACHE_NAME = "static-cache-v1";
 const DATA_CACHE_NAME = "data-cache-v1";
+
+self.addEventListener("install", function (evt) {
+    evt.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            console.log("Files cached");
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+
+    self.skipWaiting();
+});
+
+// activate
+self.addEventListener("activate", function (evt) {
+    evt.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        console.log("Old cash data removed", key);
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+
+    self.clients.claim();
+});
