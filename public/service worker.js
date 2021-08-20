421070f775f4a77fab3fcdf7a948ccf550dea60b
Josh Gumperz <joshg3023@gmail.com>
2021-08-19 23:30:28 -0700

    added service-worker.js

---
+++ public/service worker.js
@@ -0,0 +1,12 @@
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/styles.css",
+    "/dist/app.bundle.js",
+    "/dist/icon_192x192.png",
+    "/dist/icon_512x512.png",
+    '/dist/manifest.json'
+];
+
+const CACHE_NAME = "static-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
